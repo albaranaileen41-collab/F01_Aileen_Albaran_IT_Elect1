34dfe2ba034d8a458f011acf8ab2613fe161f561
albaranaileen41-collab <imong email nga g gamit sa github>
2025-09-20 23:52:50 +0800

    MidAct1

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//App.js
+import {SafeAreaView} from 'react-native'
+import CommentApp from './CommentApp'
+import Messenger from './Messenger'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return(
+    <SafeAreaView style={{flex: 1}}>
+      <Messenger/>
+      <CommentApp/>
+    </SafeAreaView>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
